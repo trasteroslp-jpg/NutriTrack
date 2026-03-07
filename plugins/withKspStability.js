24706a281c06171fe23c84ff6c06b839360050d3
trasteroslp-jpg <trasteroslp@gmail.com>
2026-03-07 00:52:54 +0000

    Fix: Remove in-process KSP strategy and enable useWorkerApi to prevent Kotlin runtime version conflicts

--- plugins/withKspStability.js
+++ plugins/withKspStability.js
@@ -8,47 +8,17 @@ module.exports = function withKspStability(config) {
         item.key !== 'kotlin.daemon.jvm.options' &&
         item.key !== 'ksp.incremental' &&
         item.key !== 'ksp.allow.all.files' &&
-        item.key !== 'kotlin.compiler.execution.strategy' &&
-        item.key !== 'org.gradle.parallel' &&
-        item.key !== 'kotlin.version'
+        item.key !== 'ksp.useWorkerApi' &&
+        item.key !== 'org.gradle.parallel'
     );
 
     config.modResults.push(
-      {
-        type: 'property',
-        key: 'org.gradle.jvmargs',
-        value: '-Xmx6144m -XX:MaxMetaspaceSize=1024m -XX:+UseParallelGC',
-      },
-      {
-        type: 'property',
-        key: 'kotlin.daemon.jvm.options',
-        value: '-Xmx3072m',
-      },
-      {
-        type: 'property',
-        key: 'ksp.incremental',
-        value: 'false',
-      },
-      {
-        type: 'property',
-        key: 'ksp.allow.all.files',
-        value: 'true',
-      },
-      {
-        type: 'property',
-        key: 'kotlin.compiler.execution.strategy',
-        value: 'in-process',
-      },
-      {
-        type: 'property',
-        key: 'org.gradle.parallel',
-        value: 'false',
-      },
-      {
-        type: 'property',
-        key: 'kotlin.version',
-        value: '2.0.21',
-      }
+      { type: 'property', key: 'org.gradle.jvmargs', value: '-Xmx6144m -XX:MaxMetaspaceSize=1024m -XX:+UseParallelGC' },
+      { type: 'property', key: 'kotlin.daemon.jvm.options', value: '-Xmx3072m' },
+      { type: 'property', key: 'ksp.incremental', value: 'false' },
+      { type: 'property', key: 'ksp.allow.all.files', value: 'true' },
+      { type: 'property', key: 'ksp.useWorkerApi', value: 'true' },
+      { type: 'property', key: 'org.gradle.parallel', value: 'false' }
     );
 
     return config;
